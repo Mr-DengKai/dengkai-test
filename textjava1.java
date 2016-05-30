490cdfc60bab24c4fa469217f4ba40abc0b34bf2
Mr-DengKai <964809903@qq.com>
2016-05-30 08:19:29 +0800

    add textjava1.java

---
+++ textjava1.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+import java.util.Calendar;
+public class demo1{
+public static void main(String[] args){
+         Date time = new Date();
+	 System.out.println(time.getTime());
+}
+
+}
